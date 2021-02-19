5dd739b21298c60b1517b7cc092a1e8a8ccbe752
Vladislav <grib9544@gmail.com>
2021-02-19 20:11:49 +0300

    init commit

---
+++ src/test/java/SomeTests.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeTests {
+    @Test
+    @Tag("positive")
+    void successTest() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    @Tag("negative")
+    void negativeTest() {
+        assertEquals(false, false);
+    }
+
+    @Test
+    @DisplayName("Some negative test")
+    void withStepTest() {
+        step("Some step", () -> assertThat(false, is(false)));
+    }
+}
